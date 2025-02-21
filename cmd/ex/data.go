70a0f40ff0a8edea400896fca3fc1d4e971d70be
Rickard Englund <rickard.englund@skf.com>
2025-02-21 10:17:44 +0100

    animate tw & sp

--- cmd/ex/data.go
+++ cmd/ex/data.go
@@ -5,19 +5,24 @@ import (
 	"math/cmplx"
 	"math/rand"
 
+	"github.com/gen2brain/raylib-go/easings"
 	rl "github.com/gen2brain/raylib-go/raylib"
 	"github.com/mjibson/go-dsp/fft"
 )
 
 type data struct {
-	tw []rl.Vector2
-	sp []rl.Vector2
+	tw      []rl.Vector2
+	sp      []rl.Vector2
+	prevTW  []rl.Vector2
+	updated float64
+	prevSP  []rl.Vector2
 }
 
 func newData() *data {
 	return &data{
-		tw: []rl.Vector2{},
-		sp: []rl.Vector2{},
+		tw:      []rl.Vector2{},
+		sp:      []rl.Vector2{},
+		updated: rl.GetTime(),
 	}
 }
 
@@ -40,16 +45,34 @@ func (d *data) update() {
 		spectra[i] = rl.NewVector2(f, float32(m))
 	}
 
+	d.prevTW = d.tw
+	d.updated = rl.GetTime()
 	d.tw = wave
+	d.prevSP = d.sp
 	d.sp = spectra
 }
 
+func anim(old, cur []rl.Vector2, updated, dur float32) []rl.Vector2 {
+	now := float32(rl.GetTime())
+	if now > updated+dur || len(old) != len(cur) {
+		return cur
+	}
+
+	mod := make([]rl.Vector2, len(cur))
+	for i := range cur {
+		y := easings.QuadIn(float32(now-updated), old[i].Y, cur[i].Y-old[i].Y, dur)
+		mod[i] = rl.NewVector2(cur[i].X, y)
+	}
+
+	return mod
+}
+
 func (d *data) getSP() []rl.Vector2 {
-	return d.sp
+	return anim(d.prevSP, d.sp, float32(d.updated), .2)
 }
 
 func (d *data) getTWF() []rl.Vector2 {
-	return d.tw
+	return anim(d.prevTW, d.tw, float32(d.updated), .2)
 }
 
 func getSine() []rl.Vector2 {
